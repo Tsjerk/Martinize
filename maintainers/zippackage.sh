49c96b2abfa76f20701e8dbcec9c8543099296df
Tsjerk A. Wassenaar <tsjerkw@gmail.com>
2017-04-11 07:59:48 +0200

    Added and modified zippackage from insane...

---
+++ maintainers/zippackage.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -euo pipefail
+
+# Build a zip package of martinize
+#
+# In order to be easily used from the command line without installation, while
+# being modular for developers and library users, we distribute martinize an
+# executable zip. That zip can be called directly from the command line (e.g.
+# `./martinize -h`) or through python (e.g. `python martinize -h`).
+#
+# The distributed zip is a zipped version of the repository prepended with a
+# shebang.
+
+# Path from which to download simopt
+SIMOPT_URL='https://raw.githubusercontent.com/Tsjerk/simopt/master/simopt.py'
+
+# Zip is annoying when it comes to path inside the archive.
+# We make a copy of the repository that we can modify without worrying to much.
+mkdir -p build
+rsync -ruv ../* build \
+    --exclude='tests' \
+    --exclude='maintainers' \
+    --exclude='*.egg-info' \
+    --exclude='setup.py'
+cd build
+
+# Download simopt
+wget "${SIMOPT_URL}" -O simopt.py || curl -O "${SIMOPT_URL}" 
+
+# __main__.py is the entry point of the script. We do not want it in the
+# module as it makes no sense to have it there, so we write it here.
+cat << EOF > __main__.py
+import os
+import sys
+import martinize.cli
+
+# When running the zipped package, __file__ is set to martinize.__main__.py
+# Hide the internals to the user; he does not need to know what is inside the
+# zip. If anything else than simopt uses __main__.__file__ it may be broken.
+__file__ = os.path.dirname(__file__)
+
+sys.exit(martinize.cli.main(sys.argv))
+EOF
+
+# Actually make the zip file. Finally!
+zip -r martinize.zip *
+
+# Make it executable
+echo '#!/usr/bin/env python' | cat - martinize.zip > package
+chmod +x package
+
+# Clean the mess
+cd ../
+mv build/package ./martinize
+rm -rf build
+
+# Were are done. Let's say good bye.
+echo 'The zip package is created. Try `./martinize -h`.'
